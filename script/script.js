682652839eb9bdc0da47c6c57f0cb47eda36f609
maikhamel <hamm3157@gmail.com>
2024-04-04 18:45:01 -0400

    finalisation spritesheet

--- script/script.js
+++ script/script.js
@@ -1,11 +1,33 @@
 
-let fleche1 = document.querySelectorAll('#flecheimg');
-gsap.fromTo(fleche1, {
+let fleche1 = document.querySelectorAll('.flecheimg');
+
+gsap.fromTo(['.flecheimg'], {
     filter: 'invert(0%) sepia(15%) saturate(7500%) hue-rotate(9deg) brightness(86%) contrast(105%)'
 
 },
 {
-    filter: 'invert(99%) sepia(19%) saturate(147%) hue-rotate(170deg) brightness(112%) contrast(100%)', repeat:-1, duration:1, yoyo: true, ease: "sine.inOut", stagger: {
+    filter: 'invert(99%) sepia(19%) saturate(147%) hue-rotate(170deg) brightness(112%) contrast(100%)', repeat:-1, duration:1, ease: "sine.inOut", stagger: {
         each: 0.1,
     }
 })
+
+let corps = document.querySelector(".corps");
+
+
+
+window.addEventListener('scroll', () => {
+    corps.classList.toggle("isScrolling");
+    
+    setTimeout(() => {
+        corps.classList.remove("isScrolling")
+    },100);
+});
+/*
+let minuteur = setTimeout(() => {
+    corps.classList.remove("isScrolling")
+    console.log("allo")
+},1000);
+
+*/
+
+
